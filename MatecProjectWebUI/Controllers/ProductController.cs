435fef99b9bc9e5b83156b244bb849c43dd79b7b
Asim Mutlu <asimmutlu6134@gmail.com>
2024-04-11 23:34:54 +0300

    Product Controller added and UnitType Pages edited.

--- MatecProjectWebUI/Controllers/ProductController.cs
+++ MatecProjectWebUI/Controllers/ProductController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MatecProjectWebUI.Controllers
 {
     public class ProductController : Controller
     {
+        IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
+            var values = _productService.TGetAllList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            product.Status = 1;
+            _productService.TAdd(product);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _productService.TDelete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var value = _productService.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            _productService.TUpdate(product);
+            return RedirectToAction("Index");
         }
     }
 }
